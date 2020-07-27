39d4e3f622ea1be95db9ade7926b0b39d60c9105
letecode <68450636+letecode@users.noreply.github.com>
2020-07-27 15:15:56 +0100

    Update web.php

--- web.php
+++ web.php
@@ -12,70 +12,14 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes();
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
 
-Route::get('/clean', function () {
-    Artisan::call('cache:clear');
-    Artisan::call('route:clear');
-    Artisan::call('config:clear');
-    Artisan::call('view:clear');
-    return "Cache, Route, Config and Views are cleared";
-});
 
-Route::post('newsletter','gls\NewsletterController@store');
 
 Route::prefix('{lang?}')->middleware('locale')->group(function() {
-
-    Route::get('/', 'gls\IndexController@index');
-    
-    Route::get('/about', 'gls\IndexController@about');
-    
-    Route::get('/contact', function () {
-        return view('pages.contact');
-    });
-    
-    Route::get('/services', 'gls\IndexController@services');
-        
-    
-    Route::group(['prefix' => 'portfolio'], function() {
-
-        Route::get('/', 'gls\IndexController@works'); 
-
-        Route::get('/{slug}', 'gls\IndexController@work'); 
-                    
-    });
-   
-    
-    Route::get('/testimonials', 'gls\IndexController@testimonials');
-    
-    Route::get('/team', 'gls\IndexController@teams');
-    
-    Route::get('/pricing', function () {
-        return view('pages.pricing');
-    });
-    
-    Route::get('/blog', function () {
-        return view('pages.blog');
-    });
-    
-    Route::get('/blog/{slug}', function () {
-        return view('pages.blog-details');
-    });
    
     Route::get('/home', 'HomeController@index')->name('home');
     
-    
-    Route::get('/terms-of-services', function () {
-        return view('pages.terms');
-    });
-    
-    Route::get('/privacy-policy', function () {
-        return view('pages.privacy');
-    });
 });
 
-Route::post('/contact', 'gls\ContactController@send');
+
 
